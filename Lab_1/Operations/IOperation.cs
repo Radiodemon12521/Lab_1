8f21745427e4128f8729d7426e8a464a01e8a193
Radiodemon12521 <d.a.chmut@student.khai.edu>
2022-12-07 18:56:20 +0200

    добавлен интерфейс

---
+++ Lab_1/Operations/IOperation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1.Operations
+{
+    public interface IOperation
+    {
+        public string Name { get; set; }
+
+        public double Calculate(double first,double second);
+    }
+}
