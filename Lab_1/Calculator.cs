332f1dda61e20647a4c0c6f7ddbf3381d3d7d788
Radiodemon12521 <d.a.chmut@student.khai.edu>
2022-12-07 18:57:41 +0200

    создан калькулятор

---
+++ Lab_1/Calculator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab_1.Operations;
+
+namespace Lab_1;
+
+public class Calculator
+{
+    private List<IOperation> operations;
+
+    public Calculator()
+    {
+        operations = new()
+        {
+            new Plus(),
+            new Minus(),
+            new Multiply(),
+            new Divide()
+        };
+    }
+
+    public void Run()
+    {
+        bool isCorrectInput = false;
+        double firstNum = 0;
+        double secondNum = 0;
+
+        while (!isCorrectInput)
+        {
+            Console.WriteLine("Введите первое число ");
+            var number1 = Console.ReadLine();
+            Console.WriteLine("Введите второе число ");
+            var number2 = Console.ReadLine();
+
+            var isFirstCorrect = double.TryParse(number1, out firstNum);
+            var isSecondCorrect = double.TryParse(number2, out secondNum);
+
+            isCorrectInput = isFirstCorrect && isSecondCorrect;
+
+            if (!isCorrectInput)
+            {
+                Console.WriteLine("Введите корректные числа");
+            }
+        }
+
+        foreach (var operation in operations)
+        {
+            var result = GetOperationResult(firstNum, secondNum, operation);
+            Console.WriteLine($"{operation.Name}: {result}");
+        }
+
+    }
+
+    private double GetOperationResult(double firstNumber, double secondNumber, IOperation operation)
+    {
+        return operation.Calculate(firstNumber, secondNumber);
+    }
+}
+
--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Lab_1;
+
+class Program
+{
+    public static void Main()
+    {
+        var calculator = new Calculator();
+        calculator.Run();
+    }
+}
+
+
